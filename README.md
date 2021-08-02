47230a6a11a9a32cc66e875ec73338343584f64e
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-08-02 14:02:13 +0530

    update readme & add comments

--- README.md
+++ README.md
@@ -1,2 +1,67 @@
 # service-auth-client
+
 ChatDaddy Auth SDK for Typescript
+
+## Refresh Tokens and Generating Them
+
+We recommend you use refresh tokens to generate these short lived access tokens. The refresh token is immune to password changes & prevents you from ever entering the password in plaintext. The refresh token automatically becomes invalid after **14 Days** of inactivity. 
+
+You do have to use your password to generate a refresh token.
+``` ts
+import { OAuthApi, encodeSHA256 } from '@chatdaddy/service-auth-client'
+
+const getRefreshToken = async() => {
+	const {
+      data: { refresh_token },
+    } = await oAuthApi.tokenPost({
+      phoneNumber: 85212345678, // use your own ChatDaddy registered phone number
+	  password: encodeSHA256('plaintextPassword'), // pass the SHA encoded password
+      returnRefreshToken: true,
+    })
+	return refresh_token
+}
+console.log(getRefreshToken()) // prints something like "676be3ff-8d6e-4e74-8b0a-16e769d1ee80"
+```
+
+## Generating Access Tokens and Using Them
+
+All of ChatDaddy's APIs rely on a single access point using the short lived JWT access token. The access token's schema can be read about [here](https://chatdaddy.stoplight.io/docs/openapi/repos/chatdaddy-service-auth/openapi.yaml/components/schemas/JWT).
+
+Presently, all access tokens last for **1 hour**.
+
+This SDK includes functions to easily generate and persist access tokens from refresh tokens
+``` ts
+import { OAuthApi, Scope, encodeSHA256 } from '@chatdaddy/service-auth-client'
+// create a factory that takes care of auto-renewing access tokens when they expire
+const getToken = makeAccessTokenFactory({
+	request: {
+		refreshToken: '676be3ff-8d6e-4e74-8b0a-16e769d1ee80', // example, use your own refresh token
+		scopes: [Scope.MessagesSendToAll] // only add scopes to send messages
+	},
+})
+;(async() => {
+	// enter the team ID you want to generate the token for
+	// read the section below to see how to get your team ID
+	const token = await getToken('976bf2fe-ar6e-4e74-8b0a-16e769d1ee80')
+	console.log(token)
+	// above line would print something like "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+
+	// fetch chats from whatsapp using the fetched token
+	fetch(
+		'https://api-wa.chatdaddy.tech/chats?count=20',
+		{
+			headers: { 'authorization': `Bearer ${token}` }
+		}
+	)
+})()
+
+```
+
+## Finding Out your Team ID
+
+1. Login & open ChatDaddy App from `https://app.chatdaddy.tech`
+2. Navigate to `https://app.chatdaddy.tech/settings/api`
+3. Copy team ID from there
+
+Example:
+![example](/find-team-id.png)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './OpenAPI/Auth'
 import type { verify } from 'jsonwebtoken'
+import { createHash } from 'crypto'
 import { Configuration, ConfigurationParameters, JWT, OAuthApi, RefreshTokenLoginRequest, Scope } from './OpenAPI/Auth'
 import SCOPES from './scopes.json'
 
@@ -27,6 +28,7 @@ export const generateBinaryString = (scopes: Scope[]) => {
 }
 
 let verifyJWT: typeof verify
+/** verify an access token really is valid, returns the decoded object */
 export const verifyToken = (token: string) => {
 	if (!verifyJWT) {
 		verifyJWT = require('jsonwebtoken').verify
@@ -34,19 +36,15 @@ export const verifyToken = (token: string) => {
 	const user = verifyJWT(token, PUBLIC_KEY, { algorithms: ['ES256'] }) as JWT
 	return user
 }
-
+/** decodes a JWT token and returns the included object */
 export const decodeToken = (token: string) => {
 	const comps = token.split('.')
 	const str = Buffer.from(comps[1], 'base64')?.toString('utf-8')
 	return JSON.parse(str || '') as JWT
 }
-
+/** get the expiry date of a JWT token */
 export const expiryDateOfToken = (jwt: JWT) => (new Date(jwt.exp * 1000))
-
-export const hasScope = (user: JWT, scope: Scope) => (
-	user.scope[SCOPES[scope]?.number] === '1'
-)
-
+/** get the scopes from a binary encoded string */
 export const getScopes = (binary: string) => {
 	const scopes: Scope[] = []
 	for (const scope of Object.keys(SCOPES) as Scope[]) {
@@ -57,9 +55,7 @@ export const getScopes = (binary: string) => {
 	return scopes
 }
 
-/**
- * Checks whether this JWT token data has at least one of these scopes
- */
+/** Checks whether this JWT token data has at least one of these scopes */
 export const validateUserScopes = (user: JWT, ...scopes: Scope[]) => {
 	if (!scopes.length) return { authorized: true, missingScopes: [] }
 
@@ -70,16 +66,19 @@ export const validateUserScopes = (user: JWT, ...scopes: Scope[]) => {
 	const authorized = missingScopes.length < scopes.length
 	return { authorized, missingScopes }
 }
-
+/** Options to create an access token for use in APIs */
 export type AccessTokenFactoryOptions = {
 	request: Omit<RefreshTokenLoginRequest, 'teamId'>
 	existingTokens?: string[]
 	config?: ConfigurationParameters
 }
-type TokenCache = { [_: string]: { token: Promise<string | { error: Error }>, expiresAt: Date | undefined } }
+
 export const makeAccessTokenFactory = (
 	{ request, existingTokens, config }: AccessTokenFactoryOptions
 ) => {
+
+	type TokenCache = { [_: string]: { token: Promise<string | { error: Error }>, expiresAt: Date | undefined } }
+
 	existingTokens = existingTokens || []
 	const tokenAPI = new OAuthApi(new Configuration(config || {}))
 	const tokenCache: TokenCache = 
@@ -124,3 +123,9 @@ export const makeAccessTokenFactory = (
 		return { token, expiresAt: result.expiresAt }
 	}
 }
+/** get the SHA encoded value */
+export const encodeSHA256 = (plaintext: string) => (
+	createHash('sha256')
+		.update(plaintext)
+		.digest('base64')
+)
